dbf9dcc42fce82d7828607641d7c110658a53c1d
NathaliaT07 <Nathalia.Torres975@icloud.com>
2024-07-05 14:16:17 -0500

    first commit

---
+++ clock.js
@@ -0,0 +1,37 @@
+// Get references to DOM elements
+
+
+const body = document.querySelector("body"),
+    hourhand = document.querySelector(".hour"),
+    minutehand = document.querySelector(".minute"),
+    secondhand = document.querySelector(".second"),
+    modeSwitch = document.querySelector(".mode-switch");
+
+//add aclick event listener to modeSwitch
+
+modeSwitch.addEventListener("click", () => {
+    //toggle the "dark" class on the body element
+
+    body.classList.toggle("dark")
+}) 
+
+
+
+const updateTime = () => {
+    //Get current time and calculate degrees for clock hands
+    let date = new Date(),
+        secToDeg = (date.getSeconds() / 60) * 360;
+        minToDeg = (date.getMinutes() / 60) * 360;
+        hrToDeg = (date.getHours() / 12 * 360);
+
+    //console.log(secToDeg)
+
+    secondhand.style.transform =`rotate(${secToDeg}deg)`;
+    minutehand.style.transform =`rotate(${minToDeg}deg)`;
+    hourhand.style.transform =`rotate(${hrToDeg}deg)`;
+
+}
+
+
+setInterval(updateTime,1000);
+updateTime();
